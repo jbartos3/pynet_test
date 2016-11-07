f4c5dec1bb9380fa125f3ac761f59a975fac334c
John Bartos <jbartos@its.jnj.com>
2016-11-07 08:36:48 -0800

    Adding my_file.py

---
+++ strex1.py
@@ -0,0 +1,8 @@
+name1 = 'Huey'
+name2 = 'Dewey'
+name3 = 'Louie'
+name4 = raw_input('Who is the head duck?: ')
+print '{:>30}'.format(name1)
+print '{:>30}'.format(name2)
+print '{:>30}'.format(name3)
+print '{:>30}'.format(name4)
